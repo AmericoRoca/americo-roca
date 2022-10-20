6a68ffb35ed86f47edd89fb8c500fe2b6a0ce3fd
Américo <americochiclana@gmail.com>
2022-10-20 16:40:05 +0200

    oops

--- src/app/components/proyects/proyects.component.ts
+++ src/app/components/proyects/proyects.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import { faFaceSadTear } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-proyects',
   templateUrl: './proyects.component.html',
   styleUrls: ['./proyects.component.css']
 })
 export class ProyectsComponent implements OnInit {
+
+  faSadTear = faFaceSadTear;
 
   constructor() { }
 
